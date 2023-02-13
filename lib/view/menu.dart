2e4b19d080235ec522c669b775fa68f2414a1064
Alexander Ruehl <alexander@dieruehls.de>
2023-02-13 20:27:27 +0100

    reenable image

--- lib/view/menu.dart
+++ lib/view/menu.dart
@@ -1,16 +1,24 @@
 import 'package:dart/view/hundredseventy.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Menu extends StatelessWidget {
   const Menu({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Image image =
+        Image.asset('assets/images/logo.png', width: 360, fit: BoxFit.fitWidth);
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 17, 17, 17),
       body: Center(
         child: Column(
           children: [
+            image,
             const SizedBox(height: 10),
             OutlinedButton(
               onPressed: () {
