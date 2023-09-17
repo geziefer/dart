567c11eed9261e66f27a647b0eab38b4f55f9a8e
Alexander Ruehl <alexander@dieruehls.de>
2023-09-17 13:32:44 +0200

    correct text size of pre-defined results

--- lib/widget/numpad.dart
+++ lib/widget/numpad.dart
@@ -50,7 +50,7 @@ class Numpad extends StatelessWidget {
                     children: [
                       for (int i = 7; i <= 9; i++)
                         _buildNumpadButton(
-                            context, controller, i.toString(), i),
+                            context, controller, i.toString(), i, true),
                     ],
                   ),
                 ),
@@ -63,7 +63,7 @@ class Numpad extends StatelessWidget {
                     children: [
                       for (int i = 4; i <= 6; i++)
                         _buildNumpadButton(
-                            context, controller, i.toString(), i),
+                            context, controller, i.toString(), i, true),
                     ],
                   ),
                 ),
@@ -74,7 +74,8 @@ class Numpad extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
                     for (int i = 1; i <= 3; i++)
-                      _buildNumpadButton(context, controller, i.toString(), i),
+                      _buildNumpadButton(
+                          context, controller, i.toString(), i, true),
                   ],
                 ),
               ),
@@ -84,9 +85,9 @@ class Numpad extends StatelessWidget {
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    _buildNumpadButton(context, controller, '↶', -2),
-                    _buildNumpadButton(context, controller, '0', 0),
-                    _buildNumpadButton(context, controller, '↵', -1),
+                    _buildNumpadButton(context, controller, '↶', -2, true),
+                    _buildNumpadButton(context, controller, '0', 0, true),
+                    _buildNumpadButton(context, controller, '↵', -1, true),
                   ],
                 ),
               ),
@@ -100,7 +101,8 @@ class Numpad extends StatelessWidget {
 
   /// build 1 button which calls Numbad controller handler
   Widget _buildNumpadButton(BuildContext context, NumpadController controller,
-      String label, int value) {
+      String label, int value, bool large) {
+    double fontSize = large ? 50 : 40;
     return Expanded(
       flex: 1,
       child: Container(
@@ -115,7 +117,7 @@ class Numpad extends StatelessWidget {
           ),
           child: Text(
             label,
-            style: const TextStyle(fontSize: 50, color: Colors.white),
+            style: TextStyle(fontSize: fontSize, color: Colors.white),
             textAlign: TextAlign.center,
           ),
         ),
@@ -140,11 +142,11 @@ class Numpad extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  _buildNumpadButton(context, controller, '26', 26),
-                  _buildNumpadButton(context, controller, '41', 41),
-                  _buildNumpadButton(context, controller, '45', 45),
-                  _buildNumpadButton(context, controller, '55', 55),
-                  _buildNumpadButton(context, controller, '60', 60),
+                  _buildNumpadButton(context, controller, '26', 26, false),
+                  _buildNumpadButton(context, controller, '41', 41, false),
+                  _buildNumpadButton(context, controller, '45', 45, false),
+                  _buildNumpadButton(context, controller, '55', 55, false),
+                  _buildNumpadButton(context, controller, '60', 60, false),
                 ],
               ),
             ),
@@ -153,11 +155,11 @@ class Numpad extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  _buildNumpadButton(context, controller, '81', 81),
-                  _buildNumpadButton(context, controller, '85', 85),
-                  _buildNumpadButton(context, controller, '100', 100),
-                  _buildNumpadButton(context, controller, '140', 140),
-                  _buildNumpadButton(context, controller, '180', 180),
+                  _buildNumpadButton(context, controller, '81', 81, false),
+                  _buildNumpadButton(context, controller, '85', 85, false),
+                  _buildNumpadButton(context, controller, '100', 100, false),
+                  _buildNumpadButton(context, controller, '140', 140, false),
+                  _buildNumpadButton(context, controller, '180', 180, false),
                 ],
               ),
             ),
